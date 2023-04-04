6c25d3a753866660d0ae9551ae90e5e3badaa428
jlondonob <j.londonobo@gmail.com>
2023-04-04 13:14:59 -0500

    🔥 Remove authentication procedure

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-import streamlit_authenticator as stauth
-import yaml
 from real_estate.finca_raiz import main, search
-from yaml import SafeLoader
 from shapely import wkt
 
 COLUMNS_TO_SHOW = [
@@ -26,78 +23,68 @@ COLUMNS_TO_SHOW = [
 def format_option(option):
     return option.replace("-", " ").title()
 
-with open('credentials.yaml') as file:
-    config = yaml.load(file, Loader=SafeLoader)
 
-authenticator = stauth.Authenticate(
-    config['credentials'],
-    config['cookie']['name'],
-    config['cookie']['key'],
-    config['cookie']['expiry_days'],
-    config['preauthorized']
+st.write("# Human Real Estate - Colombia 🏠🇨🇴")
+
+text_input = st.text_input("Search with a keyword", "La Estrella, Antioquia")
+
+st.sidebar.write("## Select your options")
+property_types = st.sidebar.multiselect(
+    "Select property types",
+    search.PROPERTY_TYPES,
+    default="apartment",
+    format_func=format_option,
+)
+offers = st.sidebar.multiselect(
+    "Select type of offer",
+    search.OFFERS,
+    default="rent",
+    format_func=format_option,
 )
 
-name, authentication_status, username = authenticator.login('Login', 'main')
-if authentication_status:
-    st.write("# Human Real Estate - Colombia 🏠🇨🇴")
+sc = main.FincaRaizClient(1000)
 
-    text_input = st.text_input("Search with a keyword", "La Estrella, Antioquia")
+total_properies = sc.total_listings(
+    text_input, offer=offers, property_type=property_types
+)
+st.metric("Total properties under this search", f"{total_properies:,}")
 
-    st.sidebar.write("## Select your options")
-    property_types = st.sidebar.multiselect(
-        "Select property types",
-        search.PROPERTY_TYPES,
-        default="apartment",
-        format_func=format_option,
+if len(property_types) == 1 and len(offers) == 1:
+    df = sc.search(text_input, offer=offers, property_type=property_types)
+    df["geometry"] = df["locations.location_point"].apply(wkt.loads)
+    df["lat"] = df["geometry"].apply(lambda p: p.y)
+    df["lon"] = df["geometry"].apply(lambda p: p.x)
+    df["lat"] = df["lat"].where(df["lat"].between(4, 11), pd.NA)
+    df["lon"] = df["lon"].where(df["lon"].between(-76, -73), pd.NA)
+
+    df["price_m2"] = pd.to_numeric(df["price_m2"])
+    min = df["price_m2"].quantile(0.05)
+    max = df["price_m2"].quantile(0.95)
+    df["price_m2"] = df["price_m2"].where(
+        (df["price_m2"] > min) & (df["price_m2"] < max), pd.NA
     )
-    offers = st.sidebar.multiselect(
-        "Select type of offer",
-        search.OFFERS,
-        default="rent",
-        format_func=format_option,
+
+    st.write("## Properties characteristics demo")
+    st.write(f"_{len(df)} properties_")
+    st.dataframe(df.filter(COLUMNS_TO_SHOW))
+
+    st.write("### Locations")
+    st.map(data=df.dropna(subset=["lat", "lon"]), zoom=5)
+
+    st.plotly_chart(px.histogram(df, x="price_m2", title="Price m<sup>2</sup>"))
+    st.plotly_chart(px.histogram(df, x="rooms.name", title="Number of rooms"))
+    st.plotly_chart(px.histogram(df, x="baths.name", title="Number of bathrooms"))
+
+    strat = px.histogram(df, x="stratum.name", title="Stratum")
+    strat.update_xaxes(
+        categoryorder="array",
+        categoryarray=[
+            "Estrato 1",
+            "Estrato 2",
+            "Estrato 3",
+            "Estrato 4",
+            "Estrato 5",
+            "Estrato 6",
+        ],
     )
-
-    sc = main.FincaRaizClient(1000)
-
-    total_properies = sc.total_listings(
-        text_input, offer=offers, property_type=property_types
-    )
-    st.metric("Total properties under this search", f"{total_properies:,}")
-
-    if len(property_types) == 1 and len(offers) == 1:
-        df = sc.search(text_input, offer=offers, property_type=property_types)
-        df["geometry"] = df["locations.location_point"].apply(wkt.loads)
-        df["lat"] = df["geometry"].apply(lambda p: p.y)
-        df["lon"] = df["geometry"].apply(lambda p: p.x)
-        df["lat"] = df["lat"].where(df["lat"].between(4, 11), pd.NA)
-        df["lon"] = df["lon"].where(df["lon"].between(-76, -73), pd.NA)
-        
-        df["price_m2"] = pd.to_numeric(df["price_m2"])
-        min = df["price_m2"].quantile(0.05)
-        max = df["price_m2"].quantile(0.95)
-        df["price_m2"] = df["price_m2"].where(
-            (df["price_m2"] > min) & (df["price_m2"] < max), pd.NA
-        )
-        
-        
-        st.write("## Properties characteristics demo")
-        st.write(f"_{len(df)} properties_")
-        st.dataframe(df.filter(COLUMNS_TO_SHOW))
-        
-        
-        st.write("### Locations")
-        st.map(data=df.dropna(subset=["lat", "lon"]), zoom=5)
-        
-
-        st.plotly_chart(px.histogram(df, x="price_m2", title="Price m<sup>2</sup>"))
-        st.plotly_chart(px.histogram(df, x="rooms.name", title="Number of rooms"))
-        st.plotly_chart(px.histogram(df, x="baths.name", title="Number of bathrooms"))
-        
-        strat = px.histogram(df, x="stratum.name", title="Stratum")
-        strat.update_xaxes(categoryorder="array", categoryarray=["Estrato 1", "Estrato 2", "Estrato 3", "Estrato 4", "Estrato 5", "Estrato 6"])
-        st.plotly_chart(strat)
-
-elif authentication_status is False:
-    st.error('Username/password is incorrect')
-elif authentication_status is None:
-    st.warning('Please enter your username and password')
+    st.plotly_chart(strat)
