3318632beecb2b594ecbb620905e0fa55477d16b
jlondonob <j.londonobo@gmail.com>
2023-04-05 01:12:12 -0500

    🎨 Improve about page

--- eterna-primavera/0_👋🏻_Bienvenido.py
+++ eterna-primavera/0_👋🏻_Bienvenido.py
@@ -5,8 +5,15 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.markdown("# Eterna Primavera: Un Análisis de Bienes Raíces en Medellín 🌬🏡🌳")
-with st.expander("¿Por qué Eterna Primavera?"):
+st.markdown(
+    """
+    <h1 style="text-align: center; padding: 5px;">Eterna Primavera</h1>
+    <h4 style="text-align: center; padding: 5px;">Un Análisis de Bienes Raíces en Medellín, Colombia</h4>
+    <h4 style="text-align: center; padding-top: 5px; padding-bottom:20px;">🌬🏡🌳</h4>
+    """,
+    unsafe_allow_html=True
+)
+with st.expander("¿Por qué Medellín?"):
     st.markdown(
         """
         Por qué no?
