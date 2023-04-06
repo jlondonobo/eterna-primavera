27f9d434769cad970fccc21c9f87dac25e3ebb86
jlondonobo <j.londonobo@gmail.com>
2023-04-05 23:22:22 -0500

    🚧 City descriptions

---
+++ eterna-primavera/texts.py
@@ -0,0 +1,57 @@
+from cities import City
+
+TEXTS = {
+    City.medellin: """
+    Medellín es una ciudad y municipio colombiano ubicado en la región andina del país.
+    Es la capital del departamento de Antioquia y la segunda ciudad más poblada de Colombia, después de Bogotá.
+    Con una población de aproximadamente 2,5 millones de habitantes,
+    Medellín es una ciudad vibrante y cosmopolita que ofrece una gran variedad de atracciones turísticas y culturales.
+
+    Una de las características más notables de Medellín es su ubicación geográfica.
+    La ciudad se encuentra en un valle rodeado por las montañas de la Cordillera Central de los Andes,
+    lo que le brinda un clima templado durante todo el año y una vista espectacular de los alrededores.
+
+    Desde la perspectiva del mercado de bienes raíces, Medellín es una ciudad en constante
+    crecimiento y evolución. La ciudad ha experimentado un auge en el sector
+    inmobiliario en los últimos años, gracias a la estabilidad económica, a una población
+    en constante crecimiento y a un aumento significativo del turismo proveniente del exterior.
+    
+    Medellín es el centro financiero, cultural y comercial de la región,
+    y cuenta con la mayor oferta de servicios y opciones de entretenimiento. 
+    """,
+    City.bello: """
+    El municipio de Bello, Antioquia, se caracteriza por tener una amplia variedad de opciones de vivienda, desde pequeñas casas de barrio hasta grandes edificios de apartamentos. Bello es uno de los municipios más poblados del área metropolitana del Valle de Aburrá y ha experimentado un notable crecimiento urbano en las últimas décadas.
+    
+    En cuanto a las viviendas de Bello, la mayoría de las casas son de tamaño medio y están construidas en ladrillo, con techos de tejas y patios traseros. Las viviendas pueden variar en cuanto a su diseño y distribución, pero en general son funcionales y cuentan con los servicios básicos necesarios.
+    
+    En general, el mercado de vivienda en Bello es bastante accesible en términos de precios, en comparación con otras zonas del Valle de Aburrá, lo que la hace una opción atractiva para aquellos que buscan vivienda en una zona urbana, con fácil acceso a servicios y transportes públicos. No obstante, es importante tener en cuenta que como en cualquier zona urbana, la seguridad y la congestión vehicular pueden ser un desafío en ciertas áreas.
+    """,
+    City.itagui: """
+    Una de las principales características de Itagüí es su ubicación estratégica, lo que le permite tener fácil acceso a otras zonas del Valle de Aburrá y a la ciudad de Medellín. El municipio está conectado por varias vías principales, incluyendo la Autopista Sur y la Avenida Regional, lo que facilita la movilidad de sus habitantes.
+    
+    Itagüí es un municipio que se ha caracterizado por tener una importante población trabajadora, y por lo tanto, una gran cantidad de viviendas asequibles para la clase media. El municipio cuenta con una amplia oferta de viviendas con precios accesibles, lo que lo hace una opción atractiva para muchas personas que buscan una opción económica para vivir en el área metropolitana del Valle de Aburrá.
+    """,
+    City.envigado: """
+    Envigado es conocido por su ambiente tranquilo y seguro y por su excelente calidad de vida.
+
+    A pesar de ser un municipio independiente, Envigado está ubicado muy cerca de Medellín,
+    lo que lo convierte en una excelente opción para aquellas personas que trabajan en la
+    ciudad y buscan un lugar más tranquilo para vivir.
+    
+    Envigado cuenta con una amplia oferta de servicios,
+    como centros comerciales, supermercados, restaurantes y cafés,
+    lo que facilita el día a día de los habitantes.
+    """,
+    City.caldas: """
+    Caldas es un municipio que está ubicado en la ladera occidental del valle de Aburrá, lo que lo convierte en un lugar con un clima agradable y fresco, ideal para quienes buscan vivir en contacto con la naturaleza y respirar aire puro.
+    
+    En comparación con otras zonas del valle de Aburrá, el costo de vida en Caldas es relativamente bajo, lo que permite a las personas vivir de manera más cómoda y ahorrar más dinero.
+    
+    En Caldas se pueden observar viviendas modestas en algunas zonas adyacentes al centro del pueblo y al parque principal. No obstante, en las montañas circundantes se pueden encontrar construcciones más ostentosas, como mansions y casas de campo.
+    """,
+    City.sabaneta: "",
+    City.copacabana: "",
+    City.la_estrella: "",
+    City.girardota: "",
+    City.barbosa: "",
+}
