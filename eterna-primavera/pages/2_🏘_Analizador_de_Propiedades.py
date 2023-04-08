d7e6affe596f6a6087bc1981c1599ec33a413950
jlondonobo <j.londonobo@gmail.com>
2023-04-08 00:04:54 -0500

    Dupe

--- eterna-primavera/pages/2_🏘_Analizador_de_Propiedades.py
+++ eterna-primavera/pages/2_🏘_Analizador_de_Propiedades.py
@@ -25,9 +25,7 @@ except ModuleNotFoundError as e:
     sleep_time = 30
     st.markdown(f"Waiting {sleep_time} seconds to install human-real-estate")
 
-    subprocess.Popen(['git config credential.helper store'], shell=True)
-    subprocess.Popen(['git config --global user.name jlondonobo'], shell=True)
-    subprocess.Popen([f'{sys.executable} -m pip install real-estate@git+https://jlondonobo:{{token}}@github.com/HumanLD/human-real-estate'], shell=True)
+    subprocess.Popen([f'{sys.executable} -m pip install real-estate@git+https://jlondonobo:${{token}}@github.com/HumanLD/human-real-estate'], shell=True)
     time.sleep(30)
 
 from real_estate.finca_raiz import main, search
